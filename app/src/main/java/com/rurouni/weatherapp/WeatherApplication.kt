1ea04a22c727fc845f8d05d07fec442f3b008aba
Enes Yilmaz <85996021+Prometheum01@users.noreply.github.com>
2024-09-02 21:05:53 +0300

    some view bug fixed

--- app/src/main/java/com/rurouni/weatherapp/WeatherApplication.kt
+++ app/src/main/java/com/rurouni/weatherapp/WeatherApplication.kt
@@ -6,9 +6,6 @@ import dagger.hilt.android.HiltAndroidApp
 @HiltAndroidApp
 class WeatherApplication : Application()
 
-//pull to refresh ekle
-//Location yoksa simgeyi duzelt
-
 //iconlari renkli ikonlarla degistir
 //Lottie animasyonlari ekle
 
@@ -16,5 +13,3 @@ class WeatherApplication : Application()
     //expanded ve collapse modda textler uzuyor, ayrica collapse modda image kuculuyor
     //Gunluk listede bazen duzen bozuluyor ona bak
     //En asagiya polen vb degerleri ekle
-
-//Room veritabani ekle
